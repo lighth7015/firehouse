98d141967dc193061477a074d68dce1c1f478078
Jason Miller <developit@users.noreply.github.com>
2020-04-20 14:01:04 -0400

    remove default entrypoint configuration

--- preact.config.js
+++ preact.config.js
@@ -23,12 +23,5 @@ export default {
                 silent: true
             });
         });
-
-        // Use any `index` file, not just index.js
-        config.resolve.alias["preact-cli-entrypoint"] = resolve(
-            process.cwd(),
-            "src",
-            "index"
-        );
     }
 };
